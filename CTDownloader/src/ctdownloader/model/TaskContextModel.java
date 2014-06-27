788a5e6a265bbd9aff4758a0fcbaf6a092a09e44
unknown <Katja.Kevic@uzh.ch>
2014-06-27 10:34:13 +0200

    added task context methods

--- CTDownloader/src/ctdownloader/model/TaskContextModel.java
+++ CTDownloader/src/ctdownloader/model/TaskContextModel.java
@@ -1,18 +1,68 @@
 package ctdownloader.model;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import org.eclipse.mylyn.context.core.IInteractionContext;
+import org.eclipse.mylyn.context.core.IInteractionElement;
+import org.eclipse.mylyn.monitor.core.InteractionEvent;
+
+import ctdownloader.model.ChangeTaskModel.CustomComparator;
+import ctdownloader.model.InteractionEventModel.EventKind;
+import ctdownloader.model.codeelements.MethodModel;
 
 public class TaskContextModel {
-	
+
 	private ArrayList<InteractionEventModel> interactions = new ArrayList<>();
-	
-	public TaskContextModel(ArrayList<InteractionEventModel> events){
+
+	public TaskContextModel(ArrayList<InteractionEventModel> events) {
 		interactions.addAll(events);
 	}
 
-	public ArrayList<InteractionEventModel> getInteractionEvents(){
+	public ArrayList<InteractionEventModel> getInteractionEvents() {
 		return interactions;
 	}
+
+	public ArrayList<MethodModel> getMethods() {
+		ArrayList<MethodModel> methods = new ArrayList<>();
+		for (InteractionEventModel event : interactions) {
+			if (event.getEventKind() != EventKind.PROPAGATION) {
+				if (event.getStructureHandle().contains("~")) {
+					MethodModel m = new MethodModel(event.getStructureHandle());
+					methods.add(m);
+
+				}
+			}
+
+		}
+
+		return methods;
+	}
+
 	
+	public ArrayList<InteractionEventModel> getSortedInteractions( ArrayList<InteractionEventModel> events) throws NoTaskContextAvailableException {
+		Collections.sort(events, new DateComparator());
+		return events;
+	}
 	
+	public ArrayList<InteractionEventModel> getAllButPropagations(ArrayList<InteractionEventModel> events){
+		ArrayList<InteractionEventModel> noPropagations = new ArrayList<>();
+		
+		for (InteractionEventModel interactionEventModel : events) {
+			if(interactionEventModel.getEventKind() != EventKind.PROPAGATION){
+				noPropagations.add(interactionEventModel);
+			}
+		}
+		
+		return noPropagations;
+	}
+	
+	public class DateComparator implements Comparator<InteractionEventModel> {
+		@Override
+		public int compare(InteractionEventModel o1, InteractionEventModel o2) {
+			return o1.getStartDate().compareTo(o2.getStartDate());
+		}
+	}
 }
---
+++ CTDownloader/src/ctdownloader/model/codeelements/MethodModel.java
@@ -0,0 +1,15 @@
+package ctdownloader.model.codeelements;
+
+public class MethodModel {
+	
+	private String resource;
+	
+	public MethodModel(String resource){
+		this.resource = resource;
+	}
+	
+	public String getResource(){
+		return resource;
+	}
+
+}
