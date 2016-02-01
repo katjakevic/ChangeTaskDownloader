e0b9910bed7f1530716307a9cd87c7bd15c31f24
unknown <Katja.Kevic@uzh.ch>
2016-02-01 20:39:52 +0100

    simple test case

--- CTDownloader/tests/ctdownloader/connector/test/BugzillaTaskDownloaderTest.java
+++ CTDownloader/tests/ctdownloader/connector/test/BugzillaTaskDownloaderTest.java
@@ -22,21 +22,21 @@ public class BugzillaTaskDownloaderTest extends TestCase {
 	
 	@Test
 	public void testQueryTasksWithValidURL() throws InvalidURLException{
-		ArrayList<ChangeTaskModel> tasks =  downloader.queryTasks("https://bugs.eclipse.org/bugs/buglist.cgi?classification=Mylyn&f1=attachments.filename&o1=substring&order=Importance&product=Mylyn%20Context&query_format=advanced&v1=context");
-
-		System.out.println(tasks.size());
+		//ArrayList<ChangeTaskModel> tasks =  downloader.queryTasks("https://bugs.eclipse.org/bugs/buglist.cgi?classification=Mylyn&f1=attachments.filename&o1=substring&order=Importance&product=Mylyn%20Context&query_format=advanced&v1=context");
+		ArrayList<ChangeTaskModel> tasks =  downloader.queryTasks("https://bugs.eclipse.org/bugs/buglist.cgi?bug_status=NEW&classification=Tools&component=build&email1=adrian.colyer&emailassigned_to1=1&emailtype1=substring&product=AspectJ&query_format=advanced");
+		assertEquals(1, tasks.size());
 	}
 	
-	@Test
-	public void testQueryTasksWithInValidURL() {
-		Throwable ex = null;
-		try {
-			downloader.queryTasks("buglist.cgi?classification=Mylyn&f1=attachments.description&o1=substring&product=Mylyn%20Context&query_format=advanced&v1=mylyn%2Fcontext%2Fzip");
-		} catch (InvalidURLException e) {
-			ex = e;
-		}
-		assertTrue(ex instanceof InvalidURLException);
-	}
+//	@Test
+//	public void testQueryTasksWithInValidURL() {
+//		Throwable ex = null;
+//		try {
+//			downloader.queryTasks("buglist.cgi?classification=Mylyn&f1=attachments.description&o1=substring&product=Mylyn%20Context&query_format=advanced&v1=mylyn%2Fcontext%2Fzip");
+//		} catch (InvalidURLException e) {
+//			ex = e;
+//		}
+//		assertTrue(ex instanceof InvalidURLException);
+//	}
 	
 
 }
